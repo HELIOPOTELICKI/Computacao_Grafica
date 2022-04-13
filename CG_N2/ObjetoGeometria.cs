cd323ab0ed515f3d34fea17108c7b40a21615cb6
HelioPotelicki <helio.p2000@hotmail.com>
2022-04-12 21:11:41 -0300

    identation

--- CG_N2/ObjetoGeometria.cs
+++ CG_N2/ObjetoGeometria.cs
@@ -7,56 +7,56 @@ using CG_Biblioteca;
 
 namespace gcgcg
 {
-  internal abstract class ObjetoGeometria : Objeto
-  {
-    protected List<Ponto4D> pontosLista = new List<Ponto4D>();
-
-    public ObjetoGeometria(char rotulo, Objeto paiRef) : base(rotulo, paiRef) { }
-
-    protected override void DesenharGeometria()
+    internal abstract class ObjetoGeometria : Objeto
     {
-      DesenharObjeto();
-    }
-    protected abstract void DesenharObjeto();
-    public void PontosAdicionar(Ponto4D pto)
-    {
-      pontosLista.Add(pto);
-      if (pontosLista.Count.Equals(1))
-        base.BBox.Atribuir(pto);
-      else
-        base.BBox.Atualizar(pto);
-      base.BBox.ProcessarCentro();
-    }
+        protected List<Ponto4D> pontosLista = new List<Ponto4D>();
 
-    public void PontosRemoverUltimo()
-    {
-      pontosLista.RemoveAt(pontosLista.Count - 1);
-    }
+        public ObjetoGeometria(char rotulo, Objeto paiRef) : base(rotulo, paiRef) { }
 
-    protected void PontosRemoverTodos()
-    {
-      pontosLista.Clear();
-    }
+        protected override void DesenharGeometria()
+        {
+            DesenharObjeto();
+        }
+        protected abstract void DesenharObjeto();
+        public void PontosAdicionar(Ponto4D pto)
+        {
+            pontosLista.Add(pto);
+            if (pontosLista.Count.Equals(1))
+                base.BBox.Atribuir(pto);
+            else
+                base.BBox.Atualizar(pto);
+            base.BBox.ProcessarCentro();
+        }
 
-    public Ponto4D PontosUltimo()
-    {
-      return pontosLista[pontosLista.Count - 1];
-    }
+        public void PontosRemoverUltimo()
+        {
+            pontosLista.RemoveAt(pontosLista.Count - 1);
+        }
 
-    public void PontosAlterar(Ponto4D pto, int posicao)
-    {
-      pontosLista[posicao] = pto;
-    }
+        protected void PontosRemoverTodos()
+        {
+            pontosLista.Clear();
+        }
 
-    public override string ToString()
-    {
-      string retorno;
-      retorno = "__ Objeto: " + base.rotulo + "\n";
-      for (var i = 0; i < pontosLista.Count; i++)
-      {
-        retorno += "P" + i + "[" + pontosLista[i].X + "," + pontosLista[i].Y + "," + pontosLista[i].Z + "," + pontosLista[i].W + "]" + "\n";
-      }
-      return (retorno);
+        public Ponto4D PontosUltimo()
+        {
+            return pontosLista[pontosLista.Count - 1];
+        }
+
+        public void PontosAlterar(Ponto4D pto, int posicao)
+        {
+            pontosLista[posicao] = pto;
+        }
+
+        public override string ToString()
+        {
+            string retorno;
+            retorno = "__ Objeto: " + base.rotulo + "\n";
+            for (var i = 0; i < pontosLista.Count; i++)
+            {
+                retorno += "P" + i + "[" + pontosLista[i].X + "," + pontosLista[i].Y + "," + pontosLista[i].Z + "," + pontosLista[i].W + "]" + "\n";
+            }
+            return (retorno);
+        }
     }
-  }
 }
